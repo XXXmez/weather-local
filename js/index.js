52264bcde0338e39af6253545ddc1bd221df0791
XXXmez <russian.bogd@gmail.com>
2021-12-26 05:28:40 +0300

    I started making a class for creating weather cards, and a function for calling the class

--- js/index.js
+++ js/index.js
@@ -1,12 +1,71 @@
-let id = 477795;
-const apiKey = '8de2150d7778bff876218c6b8d98f4f0';
-fetch(`https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}`)
+const cards = document.querySelectorAll('.card'),
+	  cardCity = document.querySelectorAll('.card-city'),
+	  cardCondition = document.querySelectorAll('.card-condition'),
+	  cardInfo = document.querySelectorAll('.card-info'),
+	  cardsWeather = document.querySelector('.cards-weather');
+
+let nameCity = 'Устюжна';									// city name
+const apiKey = '8de2150d7778bff876218c6b8d98f4f0';			// api key
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${nameCity}&appid=${apiKey}&lang=ru`)
 .then(resp => {
 	return resp.json();
 })
 .then(data => {
     console.log(data);
+	cardCity[0].textContent = data.name;
+	cardInfo[0].textContent = data.weather[0].description;
+	cardCondition[0].textContent = Math.floor(data.main.temp - 273);
 })
 .catch(err => {
 	console.error(err);
 });
+
+// 
+class AddCard {
+	constructor (cityName, cityCondition, cityInfo) {
+		this.cityName = cityName;
+		this.cityCondition = cityCondition;
+		this.cityInfo = cityInfo;
+
+	}
+
+	createCard () {
+		const div = document.createElement('div');
+		div.classList.add('card');
+		div.innerHTML = `
+			<div class="container">
+				<div class="card-city">
+					<span>Устюжна тест</span>
+				</div>
+				<div class="card-condition">
+					<span>19</span>
+				</div>
+				<div class="card-ico">
+					<img class="card-img" src="./file/img/img.png" alt="img">
+				</div>
+				<div class="card-info">
+					<span>Сегодня прохладно</span>
+				</div>
+			</div>
+		`;
+		cardsWeather.append(div);
+	}
+}
+
+function cardsCreate(city) {
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=ru`)
+	.then(resp => {
+		return resp.json();
+	})
+	.then(data => {
+		console.log(data);
+		cardCity[0].textContent = data.name;
+		cardInfo[0].textContent = data.weather[0].description;
+		cardCondition[0].textContent = Math.floor(data.main.temp - 273);
+	})
+	.catch(err => {
+		console.error(err);
+	});
+	}
+//cardsCreate('Устюжна');
