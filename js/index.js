c4062efe07a52dc001e5ceb85a0a8946c52001d8
Grymyl <russian.bogd@gmail.com>
2022-04-01 11:46:22 +0300

    Changed saving added cities to local storage. Added a logo to the footer. added a cross to the modal

--- js/index.js
+++ js/index.js
@@ -65,6 +65,7 @@ function createModal(code='',message='') {
 		<div class="modal-window">
 			<div class="modal-title">
 				<h4>Warning</h4>
+				<div class="modal-close"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 26 26" width="26px" height="26px"><path d="M 21.734375 19.640625 L 19.636719 21.734375 C 19.253906 22.121094 18.628906 22.121094 18.242188 21.734375 L 13 16.496094 L 7.761719 21.734375 C 7.375 22.121094 6.746094 22.121094 6.363281 21.734375 L 4.265625 19.640625 C 3.878906 19.253906 3.878906 18.628906 4.265625 18.242188 L 9.503906 13 L 4.265625 7.761719 C 3.882813 7.371094 3.882813 6.742188 4.265625 6.363281 L 6.363281 4.265625 C 6.746094 3.878906 7.375 3.878906 7.761719 4.265625 L 13 9.507813 L 18.242188 4.265625 C 18.628906 3.878906 19.257813 3.878906 19.636719 4.265625 L 21.734375 6.359375 C 22.121094 6.746094 22.121094 7.375 21.738281 7.761719 L 16.496094 13 L 21.734375 18.242188 C 22.121094 18.628906 22.121094 19.253906 21.734375 19.640625 Z"/></svg></div>
 			</div>
 			<div class="modal-text">
 				<div class="modal-code">${code}</div>
@@ -72,11 +73,11 @@ function createModal(code='',message='') {
 			</div>
 		</div>
 	`
-	modal.addEventListener('dblclick', (element) => {
-		//console.log(element.target);
+	body.append(modal);
+	const modalClose = document.querySelector('.modal-close');
+	modalClose.addEventListener('click', () => {
 		modal.parentNode.removeChild(modal)
 	})
-	body.append(modal);
 
 	inputCity.blur()
 }
@@ -103,6 +104,7 @@ function deletelocal(local){
 	const newArrSaveLocal = [...localStorage.getItem('saveLocal').split(',')];
 	newArrSaveLocal.splice(localStorage.getItem('saveLocal').split(',').indexOf(local), 1);
 	localStorage.setItem('saveLocal', newArrSaveLocal)
+	if(localStorage.getItem('saveLocal') == '') localStorage.removeItem('saveLocal');
 }
 
 function cardsCreate(city) {
